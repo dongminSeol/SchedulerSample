51abf156b9ba7218515706e48bc47633c39b2cbb
dongminSeol <30427110+dongminSeol@users.noreply.github.com>
2021-05-26 16:09:34 +0900

    commit
    commit

---
+++ ConsoleApp/ConsoleApp/ConsoleScheduler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class ConsoleScheduler
+    {
+        /// <summary>
+        /// 간격 (초) 
+        /// </summary>
+        /// <param name="hour">시</param>
+        /// <param name="min">분</param>
+        /// <param name="interval">반복간격(초)</param>
+        /// <param name="task">void:함수</param>
+        public static void IntervalInSeconds(int hour, int min, double interval, Action task)
+        {
+            interval = interval / 3600;
+            SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        }
+        /// <summary>
+        /// 간격 (분) 
+        /// </summary>
+        /// <param name="hour">시</param>
+        /// <param name="min">분</param>
+        /// <param name="interval">반복간격(분)</param>
+        /// <param name="task">void:함수</param>
+        public static void IntervalInMinutes(int hour, int min, double interval, Action task)
+        {
+            interval = interval / 60;
+            SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        }
+        /// <summary>
+        /// 간격 (시간) 
+        /// </summary>
+        /// <param name="hour">시</param>
+        /// <param name="min">분</param>
+        /// <param name="interval">반복간격(시간)</param>
+        /// <param name="task">void:함수</param>
+        public static void IntervalInHours(int hour, int min, double interval, Action task)
+        {
+            SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        }
+        /// <summary>
+        /// 간격 (일) 
+        /// </summary>
+        /// <param name="hour">시</param>
+        /// <param name="min">분</param>
+        /// <param name="interval">반복간격(일)</param>
+        /// <param name="task">void:함수</param>
+        public static void IntervalInDays(int hour, int min, double interval, Action task)
+        {
+            interval = interval * 24;
+            SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        }
+    }
+
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 간격(초)
+            // 오전 9:00에 시작하여 1 초마다 호출됩니다.
+            ConsoleScheduler.IntervalInSeconds(9,0,1,
+            () =>
+            {
+                Console.WriteLine($"간격(초)-{DateTime.Now.ToString("HHmmss")}");
+            });
+
+            // 간격(분)
+            // 오전 9:00에 시작하여 1 분마다 호출됩니다.
+            ConsoleScheduler.IntervalInMinutes(9, 0, 1,
+            () =>
+            {
+                Console.WriteLine($"간격(분)-{DateTime.Now.ToString("HHmmss")}");
+            });
+
+            // 간격(시간)
+            // 오전 9:00에 시작하여 1 시간마다 호출됩니다.
+            ConsoleScheduler.IntervalInHours(9, 0, 1,
+            () =>
+            {
+                Console.WriteLine($"간격(시간)-{DateTime.Now.ToString("HHmmss")}");
+            });
+
+            // 간격(날짜) 
+            // 오전 9:00 에 시작되며 1 일마다 호출됩니다.
+            ConsoleScheduler.IntervalInDays(9, 0, 1,
+            () =>
+            {
+                Console.WriteLine($"간격(날짜)-{DateTime.Now.ToString("HHmmss")}");
+            });
+
+            Console.ReadLine();
+
+        }
+    }
+}
